8423227a2a27c8e14106898802a3c5d1bbce6e16
Justin Stewart <Justinstewart3313@gmail.com>
2018-11-03 12:20:42 -0600

    Fix App test suite && write snapshot for App component

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import configureMockStore from "redux-mock-store";
 
-describe('App', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+import App from "./App";
+
+describe("App", () => {
+  let mockStore;
+  let initialState;
+  let store;
+  beforeEach(() => {
+    mockStore = configureMockStore();
+    initialState = {
+      movies: [],
+      music: [],
+      tvShow: []
+    };
+    store = mockStore(initialState);
+  });
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
-  it('should match snapshot', () => {
-    
-  })
-})
+  it("should match snapshot", () => {
+    const wrapper = shallow(<App store={store} />)
+    expect(wrapper).toMatchSnapshot();
+  });
+});
