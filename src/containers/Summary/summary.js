3d3618e8131b63f9d74faaae3afce914405c5d18
Justin Stewart <Justinstewart3313@gmail.com>
2018-11-05 10:14:47 -0700

    Display instructions and invites

--- src/containers/Summary/summary.js
+++ src/containers/Summary/summary.js
@@ -8,6 +8,8 @@ export const Summary = (props) => {
       <h2>Movies: {props.movieCount} </h2>
       <h2>Music: {props.musicCount} </h2>
       <h2>Shows: {props.tvShowCount} </h2>
+      <p className='how'>HOW TO PLAY: Each month their will be a new list of hot titles for you to check out, encouraging anyone on the water to stay off of social media and expand your knowledge on new movies, music and tv-shows.</p>
+      <p className='invite'>Invite your friends and win challenges by keeping your watch count sky high!</p>
     </div>
   )
 }
